9232650ca9f7b29bb1539582e7d12fa58bf79d6f
Oixel Romero <oycell@aol.com>
2025-11-16 13:15:25 -0500

    Add sound effects and fix food spawner

--- My project/Assets/ServingZone.cs
+++ My project/Assets/ServingZone.cs
@@ -1,30 +1,56 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
 
-public class ServingZone : MonoBehaviour {
+public class ServingZone : MonoBehaviour
+{
     private CustomerAI ownerCustomer;
     private CustomerOrder.Meal requiredFood;
 
-    public void InitializeZone(CustomerAI owner, CustomerOrder.Meal desiredFood) {
+    public AudioResource correctOrderSoundEffect;
+    public AudioResource incorrectOrderSoundEffect;
+    public AudioSource audioSource;
+
+    public void InitializeZone(CustomerAI owner, CustomerOrder.Meal desiredFood)
+    {
         ownerCustomer = owner;
         requiredFood = desiredFood;
         Debug.Log("Zone initialized, waiting for: " + desiredFood.ToString());
     }
 
+    private IEnumerator PlaySoundAndDestroy(FoodItem item, CustomerAI customer)
+    {
+        audioSource.resource = correctOrderSoundEffect;
+        audioSource.Play();
+
+        Debug.Log("Correct food delivered!");
+
+        customer.ReceiveFood(item.gameObject);
+
+        yield return new WaitForSeconds(audioSource.clip.length);
+
+        Destroy(this.gameObject);
+    }
+
     // This is called by the FoodItem when it's dropped in
-    public void TryAcceptFood(FoodItem item) {
-        if (ownerCustomer == null) {
+    public void TryAcceptFood(FoodItem item)
+    {
+        if (ownerCustomer == null)
+        {
             Debug.LogError("This zone has no owner!");
             return;
         }
 
-        if (item.GetFoodType() == requiredFood) {
-            Debug.Log("Correct food delivered!");
-
-            ownerCustomer.ReceiveFood(item.gameObject);
-
-            Destroy(this.gameObject);
-        } else {
+        if (item.GetFoodType() == requiredFood)
+        {
+            StartCoroutine(PlaySoundAndDestroy(item, ownerCustomer));
+        }
+        else
+        {
             Debug.Log("Wrong food! Wanted " + requiredFood.ToString() + ", got " + item.GetFoodType().ToString());
+
+            audioSource.resource = incorrectOrderSoundEffect;
+            audioSource.Play();
         }
     }
 }
