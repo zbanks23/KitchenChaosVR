b14ef5f3bd9e726618d549ff0c55a8bf6c21990e
Sushanth Kumar Vutukuri <sushanthvutukuri@ufl.edu>
2025-11-15 19:00:02 -0500

    point counter and point update function

--- My project/Assets/Game_Manager.cs
+++ My project/Assets/Game_Manager.cs
@@ -1,27 +1,38 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Game_Manager : MonoBehaviour
 {
     public int points;
     public int timer = 0;
     public bool first = true;
+    public TextMeshProUGUI pointDisplay;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        addPoints(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(points>500)
+        {
+            LoadScene("BreakScene");
+        }
     }
 
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void addPoints(int points)
+    {
+        this.points = points;
+        pointDisplay.text = "Points: " + points;
     }
 }
